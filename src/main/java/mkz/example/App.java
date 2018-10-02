cf65fb68ca68e2bfead4f94354185ce82fd3159c
Aleistar Markóczy <a.markoczy@gmail.com>
2018-10-02 21:01:27 +0200

    Initial

---
+++ src/main/java/mkz/example/App.java
@@ -0,0 +1,10 @@
+package mkz.example;
+
+public class App {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Runs.");
+    }
+}
